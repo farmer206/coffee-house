ed1d17ca3f3217c1339d4a596ae5eba6072eb4c9
farmer206 <farmer206th@gmail.com>
2024-02-28 16:13:24 +0300

    feat: create home and menu pages

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './scss/main.scss';
+
+const mult = (a, b) => a * b;
+
+console.log(mult(2, 4));
---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+
+module.exports = {
+  mode,
+  target,
+  devtool,
+  devServer: {
+    open: true,
+    hot: true,
+    static: {                               
+      directory: path.join(__dirname, './'),  
+      watch: true
+    }
+  },
+  entry: {
+    bundle: path.resolve(__dirname, 'src', 'index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+    assetModuleFilename: 'assets/[name][ext]'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src', 'template.html')
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'menu_page.html',
+      template: path.resolve(__dirname, 'src', 'menu_page.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: devMode ? "[name].css" : "[name].[contenthash].css",
+      chunkFilename: devMode ? "[id].css" : "[id].[contenthash].css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          "postcss-loader",
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.woff2?$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]'
+        }
+      },
+      {
+        test: /\.(jpe?g|png|webp|gif|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.m?js$/i,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ]
+  }
+}
